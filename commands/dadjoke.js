f2882371b1ad4895fc794b4cd03ff3626b828386
David Harris <bblldave@gmail.com>
2023-02-03 21:54:28 -0500

    Removed replit to start developing locally

--- commands/dadjoke.js
+++ commands/dadjoke.js
@@ -1,6 +1,7 @@
-const axios = require("axios");
+const axios = require('axios');
 const { SlashCommandBuilder } = require('discord.js');
- 
+const { DadJokeApiKey, DadJokeHost } = require('../config.json');
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('dadjoke')
@@ -10,20 +11,18 @@ module.exports = {
       method: 'GET',
       url: 'https://dad-jokes.p.rapidapi.com/random/joke',
       headers: {
-        'X-RapidAPI-Key': process.env['DadJokeApiKey'],
-        'X-RapidAPI-Host': process.env['DadJokeHost']
-      }
+        'X-RapidAPI-Key': DadJokeApiKey,
+        'X-RapidAPI-Host': DadJokeHost,
+      },
     };
-    
-    axios.request(options).then(async function (response) {
-    	const punchline = response.data.body[0].punchline;
+
+    axios.request(options).then(async function(response) {
+      const punchline = response.data.body[0].punchline;
       const setupMessage = response.data.body[0].setup;
 
       await interaction.reply(`${setupMessage} \n ${punchline}`);
-      
-    }).catch(function (error) {
-    	console.error(error);
+    }).catch(function(error) {
+      console.error(error);
     });
 	},
 };
-    
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
-const keepAlive = require('./server');
+const { token } = require('./config.json');
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds,
   GatewayIntentBits.GuildMessages,
   GatewayIntentBits.MessageContent,
-  GatewayIntentBits.GuildMembers]
+  GatewayIntentBits.GuildMembers],
 });
 
 client.commands = new Collection();
@@ -21,7 +21,8 @@ for (const file of commandFiles) {
   // Set a new item in the Collection with the key as the command name and the value as the exported module
   if ('data' in command && 'execute' in command) {
     client.commands.set(command.data.name, command);
-  } else {
+  }
+  else {
     console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
   }
 }
@@ -34,10 +35,10 @@ for (const file of eventFiles) {
 	const event = require(filePath);
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args));
-	} else {
+	}
+  else {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
 
-keepAlive();
-client.login(process.env['TOKEN']);
+client.login(token);
