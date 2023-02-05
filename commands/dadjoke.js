fbc5fbd0a85167cfcd76e62361d7b0e66bc28481
David Harris <bblldave@gmail.com>
2023-02-05 18:28:44 -0500

    Change api and removed axios

--- commands/dadjoke.js
+++ commands/dadjoke.js
@@ -1,42 +1,28 @@
-const axios = require("axios");
 const { SlashCommandBuilder } = require("discord.js");
-require("dotenv").config();
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("dadjoke")
     .setDescription("Tells a random dad joke."),
   async execute(interaction) {
-    const options = {
-      method: "GET",
-      url: "https://dad-jokes.p.rapidapi.com/random/joke",
-      headers: {
-        "X-RapidAPI-Key": process.env["DadJokeApiKey"],
-        "X-RapidAPI-Host": process.env["DadJokeHost"],
-      },
-    };
-
-    axios
-      .request(options)
-      .then(async function (response) {
-        const punchline = response.data.body[0].punchline;
-        const setupMessage = response.data.body[0].setup;
-
-        await interaction
-          .reply(`${setupMessage} \n ${punchline}`)
-          .then(async () => {
-            if (isProfanity(`${setupMessage} \n ${punchline}`)) {
-              await interaction.deleteReply().then(async () => {
-                await interaction.channel.send(
-                  "The joke contained profanity and was removed. Please try again."
-                );
-              });
-            }
-          });
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    const result = await getDadJoke();
+    if (!result.ok) {
+      await interaction.reply(
+        `I don't know any dad jokes right now. Try again in a few minutes.`
+      );
+      return;
+    }
+    const data = await result.json();
+    const joke = data.joke;
+    await interaction.reply(`${joke}`).then(async () => {
+      if (isProfanity(`${joke}`)) {
+        await interaction.deleteReply().then(async () => {
+          await interaction.channel.send(
+            "The joke contained profanity and was removed. Please try again."
+          );
+        });
+      }
+    });
   },
 };
 
@@ -44,4 +30,12 @@ function isProfanity(str) {
   const list = require("badwords-list");
   let badwords = list.array;
   return badwords.some((word) => str.split(" ").includes(word));
+}
+
+async function getDadJoke() {
+  const apiRoute = "https://icanhazdadjoke.com/";
+  const result = await fetch(apiRoute, {
+    headers: { Accept: "application/json" },
+  });
+  return result;
 }
