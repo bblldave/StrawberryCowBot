fcbcb23579fb655b47ef0558564097812e884fb0
David Harris <bblldave@gmail.com>
2023-02-16 23:02:53 -0500

    Fixed typo

--- events/modalResponse.js
+++ events/modalResponse.js
@@ -6,9 +6,7 @@ module.exports = {
     if (!interaction.isModalSubmit()) return;
     if (interaction.customId === "testerApplication") {
       const patternApplicationChannel =
-        interaction.member.guild.patternApplicationChannels.cache.get(
-          "1075982628286648382"
-        );
+        interaction.member.guild.channels.cache.get("1075982628286648382");
       const message = createMessage(interaction);
 
       await patternApplicationChannel
