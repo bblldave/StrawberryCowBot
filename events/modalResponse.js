b7fd897f401e5e202928d0965682a9836ec6454d
David Harris <bblldave@gmail.com>
2023-02-16 22:36:27 -0500

    Setup the modal response to send a message
    CU-863g22ebn

--- events/modalResponse.js
+++ events/modalResponse.js
@@ -4,28 +4,42 @@ module.exports = {
   name: Events.InteractionCreate,
   async execute(interaction) {
     if (!interaction.isModalSubmit()) return;
-
     if (interaction.customId === "testerApplication") {
-      const user = interaction.member.user.username;
-      console.log(user);
-      const name = interaction.fields.getTextInputValue("nameInput");
-      const email = interaction.fields.getTextInputValue("emailInput");
-      const socialHanle = interaction.fields.getTextInputValue("socialInput");
-      const imageUrl = interaction.fields.getTextInputValue("imageInput");
-      const confirmation = interaction.fields.getTextInputValue("confirmInput");
-      //format message
-      const message = `${user} submitted an application. 
-      Name: ${name}
-      Email: ${email}
-      Social: ${socialHanle}
-      Recent work: ${imageUrl}
-      Confirmation: ${confirmation}`;
+      const patternApplicationChannel =
+        interaction.member.guild.patternApplicationChannels.cache.get(
+          "1075982628286648382"
+        );
+      const message = createMessage(interaction);
 
-      //post to special channel
-
-      await interaction.reply({
-        content: message,
-      });
+      await patternApplicationChannel
+        .send(message)
+        .then((message) =>
+          interaction.reply(
+            `Application submitted successfully. 
+             The Tester role will be given to you if you are selected.
+            `
+          )
+        )
+        .catch(console.error);
     }
   },
 };
+
+function createMessage(interaction) {
+  const name = interaction.fields.getTextInputValue("nameInput");
+  const email = interaction.fields.getTextInputValue("emailInput");
+  const socialHanle = interaction.fields.getTextInputValue("socialInput");
+  const imageUrl = interaction.fields.getTextInputValue("imageInput");
+  const confirmation = interaction.fields.getTextInputValue("confirmInput");
+  const user = interaction.member.user.username;
+
+  const message = `
+  ${user} submitted an application. 
+  Name: ${name}
+  Email: ${email}
+  Social: ${socialHanle}
+  Recent work: ${imageUrl}
+  Confirmation: ${confirmation}`;
+
+  return message;
+}
